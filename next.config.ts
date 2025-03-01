6d29b7af491d60be94a25341f9be22228fca2ec0
Hugo Sansaqua <privet.kitty99@gmail.com>
2025-03-01 14:07:46 +0900

    Generate static site

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.SR_ENV === "production";
+const isStatic = isProd || process.env.SR_STATIC === "true";
+
+const PROD_SUBDIRECTORY = "/scenario-reduction";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: isStatic ? "export" : "standalone",
+  basePath: isProd ? PROD_SUBDIRECTORY : "",
+  assetPrefix: isProd ? PROD_SUBDIRECTORY : "",
 };
 
 export default nextConfig;
